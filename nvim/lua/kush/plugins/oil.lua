f069dd72f5c67684e0d5026b321102811a474345
waternsky <playgamesroyale2@gmail.com>
2024-06-15 18:04:30 +0530

    feat: Added harpoon

--- nvim/lua/kush/plugins/oil.lua
+++ nvim/lua/kush/plugins/oil.lua
@@ -5,6 +5,7 @@ return {
         require("oil").setup({
             default_file_explorer = true,
             delete_to_trash = true,
+            skip_confirm_for_simple_edits = true,
             view_options = { show_hidden = true },
         })
         vim.keymap.set("n", "<leader>-", require("oil").toggle_float)
